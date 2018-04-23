fb8310495e92a1759d51e27a3b40d73c3ab38c31
Alfred <renguzi@gmail.com>
2018-04-23 10:22:57 +0800

    Add files via upload

---
+++ mongoTV/mongotv_comments_crawler/mongotv_comments_crawler/main.py
@@ -0,0 +1,2 @@
+from scrapy import cmdline
+cmdline.execute("scrapy crawl crawl_comments".split())
---
+++ mongoTV/mongotv_comments_crawler/mongotv_comments_crawler/pipelines.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+from pymongo import MongoClient
+from scrapy.conf import settings
+from pymongo.errors import DuplicateKeyError
+from traceback import format_exc
+
+
+class MongotvCommentsCrawlerPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+        self.client = None
+        self.db = None
+
+    @classmethod
+    def from_crawler(cls, crawler):  # 定义类方法，提取出mongodb配置
+        return cls(
+            mongo_uri=crawler.settings.get('MONGODB_URI'),
+            mongo_db=settings.get('MONGODB_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        _ = spider
+        self.client = MongoClient(self.mongo_uri)  # 连接数据库
+        self.db = self.client[self.mongo_db]
+        self.db['mongotv1'].ensure_index('comment_id', unique=True)  # 在集合"mongotv1"中建立索引，并保证索引的唯一性
+
+    def close_spider(self, spider):
+        _ = spider
+        self.client.close()  # 关闭数据库
+
+    def process_item(self, item, spider):
+        try:
+            self.db['mongotv1'].update({'comment_id': item['comment_id']}, {'$set': item}, upsert=True)
+            # 通过comment_id判断，有就更新，没有就插入
+
+        except DuplicateKeyError:
+            spider.logger.debug('duplicate key error collection')  # 唯一键冲突报错
+        except Exception as e:
+            _ = e
+            spider.logger.error(format_exc())
+        return item
