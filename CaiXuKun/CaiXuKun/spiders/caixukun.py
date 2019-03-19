fa22bfbbc641cf23788e09d7e3ea7c12adaaeb8d
Alfred数据室 <renguzi@gmail.com>
2019-03-19 16:57:46 +0800

    Update caixukun.py

--- CaiXuKun/CaiXuKun/spiders/caixukun.py
+++ CaiXuKun/CaiXuKun/spiders/caixukun.py
@@ -8,15 +8,15 @@ class CaixukunSpider(scrapy.Spider):
     name = 'caixukun'
     allowed_domains = ['m.weibo.cn']
 
-    def start_requests(self):
+    def start_requests(self):  # 以start_requests代替strat_urls启动爬虫
         urls = ['https://m.weibo.cn/api/statuses/repostTimeline?'
-                'id=4347741368557605&page={}'.format(i) for i in range(15136)]
-        random.shuffle(urls)
+                'id=4347741368557605&page={}'.format(i) for i in range(15136)]  # 该链接通过浏览器抓包得来（微博移动端）
+        random.shuffle(urls)  # 这个api的数据是实时更新的，所以不需要按照顺序爬，shuffle一下可以增加爬虫效率
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
 
-    def parse(self, response):
+    def parse(self, response):  # 解析函数
             res = json.loads(response.text)
             if res['ok'] == 1:
                 data = res['data']['data']
