ccf28f8d3c26dd2fcb2d1fa4d30c6d781eeac149
Alfred数据室 <renguzi@gmail.com>
2019-03-29 15:27:58 +0800

    Add files via upload

---
+++ ICU996/ICU996/ICU996/spiders/a996.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import json
+import scrapy
+import pandas as pd
+
+
+class A996Spider(scrapy.Spider):
+    name = '996'
+    allowed_domains = ['github.com']
+
+    def start_requests(self):
+        data = pd.read_csv('stargazers.csv')
+        for url in data.iloc[35000:39991]['url']:
+            r_url = url + '?access_token=a1fde3da8654a01ba574919dfd32fbd2bc73e177'
+            yield scrapy.Request(url=r_url, callback=self.parse, dont_filter=True)
+
+    def parse(self, response):
+        item = json.loads(response.text)
+        if response.status == 200:
+            yield item
+        else:
+            print('链接:{}请求不成功!'.format(response.url))
